60306a7e6414fc9699f98382cf8e9f27dc4cb94d
Samet Yaşar BAYTAR <syasarbaytar@gmail.com>
2023-09-28 21:36:25 +0300

    Add project files.

---
+++ Infrastructure/ECommerceAPI.Persistence/Contexts/ECommerceAPIDbContext.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Contexts
+{
+    public class ECommerceAPIDbContext : DbContext
+    {
+        public ECommerceAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Product> Products { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<Customer> Customers { get; set; }
+        
+
+    }
+}
---
+++ Infrastructure/ECommerceAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using ECommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceServices (this IServiceCollection services)
+        {
+            services.AddDbContext<ECommerceAPIDbContext>(option => option.UseNpgsql(Configuration.ConfigurationString));
+        }
+    }
+}
